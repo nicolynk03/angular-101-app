e4339a2b38b3662f364c24e8082c68828e94cb1d
nicolynk03 <kwoknicolyn@gmail.com>
2025-08-02 17:12:22 +1000

    init commit, ready to style header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Header],
   template: `
-    <h1>Welcome to {{ title() }}!</h1>
+    <!-- <h1>Welcome to {{ title() }}!</h1>
+    <p>Hello world!</p>
 
-    <router-outlet />
+    <router-outlet /> -->
+
+    <app-header/>
+    <app-home/>
   `,
-  styles: [],
+  styles: [`
+    p {
+      background-color: blue;
+    }
+    `],
 })
 export class App {
   protected readonly title = signal('angular-101-app');
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// generates the Angular application using the app component
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
